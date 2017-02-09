ac175de3f6cf740028f02f4c8b61c548a4ebf135
Lightw3ight <kmackintosh@TradeMe.local>
2017-02-10 10:55:22 +1300

    Removed bullnose

--- src/app/api/audio-zone.service.ts
+++ src/app/api/audio-zone.service.ts
@@ -1,40 +1,40 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 export interface IAudioZone {
-	name: string,
-	path?: string
+    name: string;
+    path?: string;
 }
 
 @Injectable()
 export class AudioZoneService {
-	private zoneKey = 'currentAudioZone';
-	private _zones: IAudioZone[] = [
-		{ name: 'Bullnose', path: 'http://music.trademe.local/Bullnose' },
-		{ name: 'OML L3', path: 'http://music.trademe.local/oml/3' },
-		{ name: 'OML L4', path: 'http://music.trademe.local/oml/4' },
-		{ name: 'OML L5', path: 'http://music.trademe.local/oml/5' },
-		{ name: 'NZX L4', path: 'http://music.trademe.local/nzx/4' },
-		{ name: 'Auckland L1', path: 'http://music.trademe.local/sst/1' },
-		{ name: 'Auckland L2', path: 'http://music.trademe.local/sst/2' },
-		{name: 'Christchurch', path: 'http://chc-music.trademe.local'}
-	]
+    private zoneKey = 'currentAudioZone';
+    private _zones: IAudioZone[] = [
+//        { name: 'Bullnose', path: 'http://music.trademe.local/Bullnose' },
+        { name: 'OML L3', path: 'http://music.trademe.local/oml/3' },
+        { name: 'OML L4', path: 'http://music.trademe.local/oml/4' },
+        { name: 'OML L5', path: 'http://music.trademe.local/oml/5' },
+        { name: 'NZX L4', path: 'http://music.trademe.local/nzx/4' },
+        { name: 'Auckland L1', path: 'http://music.trademe.local/sst/1' },
+        { name: 'Auckland L2', path: 'http://music.trademe.local/sst/2' },
+        { name: 'Christchurch', path: 'http://chc-music.trademe.local' }
+    ]
 
-	getAllZones(): Promise<IAudioZone[]> {
-		return new Promise((resolve, reject) => {
-			resolve(this._zones);
-		});
-	}
-	
-	setCurrentZone(zonePath){
-		localStorage.setItem(this.zoneKey, zonePath);
-	}
-	
-	getCurrentZone(){
-		let zone = localStorage.getItem(this.zoneKey) || this._zones[0].path;
-		if (!zone.startsWith('http')){
-			zone = this._zones.find(z => z.name.indexOf(<any>zone) > 0).name;
-		}
+    getAllZones(): Promise<IAudioZone[]> {
+        return new Promise((resolve, reject) => {
+            resolve(this._zones);
+        });
+    }
 
-		return zone || this._zones[0].path;
-	}
+    setCurrentZone(zonePath) {
+        localStorage.setItem(this.zoneKey, zonePath);
+    }
+
+    getCurrentZone() {
+        let zone = localStorage.getItem(this.zoneKey) || this._zones[0].path;
+        if (!zone.startsWith('http')) {
+            zone = this._zones.find(z => z.name.indexOf(<any>zone) > 0).name;
+        }
+
+        return zone || this._zones[0].path;
+    }
 }
