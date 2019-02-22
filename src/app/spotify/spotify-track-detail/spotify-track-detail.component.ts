1c8ea616c821b00db203d6f64f3d61c3fc903b78
Gareth Bradley <gareth.bradley.nz@gmail.com>
2019-02-23 11:45:36 +1300

    Interpret key/mode

--- src/app/spotify/spotify-track-detail/spotify-track-detail.component.ts
+++ src/app/spotify/spotify-track-detail/spotify-track-detail.component.ts
@@ -13,6 +13,20 @@ export class SpotifyTrackDetailComponent implements OnInit {
   public trackDetail$;
   public audioAnalysis$;
 
+  private _keyLookup = [
+    { index: 0, musicKey: 'C'},
+    { index: 1, musicKey: 'C#'},
+    { index: 2, musicKey: 'D'},
+    { index: 3, musicKey: 'D#'},
+    { index: 4, musicKey: 'E'},
+    { index: 5, musicKey: 'F'},
+    { index: 6, musicKey: 'F#'},
+    { index: 7, musicKey: 'G'},
+    { index: 8, musicKey: 'G#'},
+    { index: 9, musicKey: 'A'},
+    { index: 10, musicKey: 'A#'},
+    { index: 11, musicKey: 'B'}
+  ];
   constructor (
     private _route: ActivatedRoute,
     private _spotifyService: SpotifyService
@@ -26,6 +40,29 @@ export class SpotifyTrackDetailComponent implements OnInit {
     this.trackDetail$ = this._spotifyService.getTrack(trackId);
 
     this.audioAnalysis$ = this._spotifyService.getAudioAnalysis(trackId);
+  }
+
+  public getMode (mode: number) {
+    if (mode === 1) {
+      return 'major';
+    } else if (mode === 0) {
+      return 'minor';
+    } else {
+      return '';
+    }
+  }
+
+  public getKey (keyIndex: number) {
+    if (!keyIndex && keyIndex !== 0) {
+      return '(unknown)';
+    }
+
+    const result = this._keyLookup.find(k => k.index === keyIndex);
+    if (!result) {
+      return '(unknown)';
+    }
+
+    return result.musicKey;
   }
 
 }
