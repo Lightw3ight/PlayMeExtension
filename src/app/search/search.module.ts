ec35526f3b183140d14fcc62c14b530dbf88f27d
overflew <gareth.bradley.nz@gmail.com>
2019-03-21 18:18:21 +1300

    Make search results easier + fix time display issue

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SearchComponent } from './search.component';
-import { MatTabsModule, MatProgressBarModule } from '@angular/material';
+import { MatTabsModule, MatProgressBarModule, MatChipsModule } from '@angular/material';
 
 @NgModule({
     imports: [
@@ -14,6 +14,7 @@ import { MatTabsModule, MatProgressBarModule } from '@angular/material';
         SharedModule,
         FlexLayoutModule,
         MatTabsModule,
+        MatChipsModule,
         ArtistListItemModule,
         AlbumListItemModule,
         MatProgressBarModule
